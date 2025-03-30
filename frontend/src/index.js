db4d88463b611f59d88e5c7ec9ac7d2ebf5483a2
Pranav Bykampadi <pranav.bykampadi@gmail.com>
2025-03-30 05:30:35 -0400

    hid api details

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
+import { auth0Config } from './config/auth0-config';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <Auth0Provider
-    domain="dev-0y61odideywbawls.us.auth0.com"
-    clientId="pA9qMGrnx2t3T3js4rWqq3A1zSiGFvSu"
+    domain={auth0Config.domain}
+    clientId={auth0Config.clientId}
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
@@ -20,7 +21,4 @@ root.render(
   </Auth0Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
